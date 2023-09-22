062840ad7286c0d70ac5879e49ea5ab9ac90bd21
theophiluskibet <kibettheophilus@gmail.com>
2023-09-22 19:18:26 +0300

    add sample grpc server

--- users/src/main/java/com/theophiluskibet/UserServiceImpl.java
+++ users/src/main/java/com/theophiluskibet/UserServiceImpl.java
@@ -9,6 +9,13 @@ import users.Users;
 public class UserServiceImpl extends UserServiceGrpc.UserServiceImplBase {
     @Override
     public void createUser(Users.UserRequest request, StreamObserver<Users.UserResponse> responseObserver) {
+        Users.UserRequest user = Users.UserRequest.newBuilder().setId(request.getId()).setName(request.getName()).build();
+
+        Users.UserResponse response = Users.UserResponse.newBuilder().setId(user.getId()).setName(user.getName()).build();
+
+        responseObserver.onNext(response);
+        responseObserver.onCompleted();
 
     }
 }
+
