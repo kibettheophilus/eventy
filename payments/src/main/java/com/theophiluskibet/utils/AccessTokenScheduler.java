650d87d6efb22719f337a8d8914ca31d68c7fbf8
theophiluskibet <kibettheophilus@gmail.com>
2023-09-25 23:39:31 +0300

    add comments

--- payments/src/main/java/com/theophiluskibet/utils/AccessTokenScheduler.java
+++ payments/src/main/java/com/theophiluskibet/utils/AccessTokenScheduler.java
@@ -12,6 +12,7 @@ import java.time.LocalDateTime;
 @Component
 public class AccessTokenScheduler {
 
+    // TODO: 25/09/2023 change time to 1hr = 1000*60*60 
     @Scheduled(fixedRate = 60 * 60)
     public void getAccessToken() {
         Request request = new Request.Builder().url("https://sandbox.safaricom.co.ke/oauth/v1/generate?grant_type=client_credentials").addHeader("Authorization", Credentials.basic("bWkjAdbz4wxma05DIsDQh4j1K79qMwAA", "iuOzK7K1fJV9BfFK")).build();
