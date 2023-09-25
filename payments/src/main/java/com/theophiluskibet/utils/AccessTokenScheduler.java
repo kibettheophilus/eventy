e2c2ae50e3bfc7981e60627ad8012f5f8e3b0e1b
theophiluskibet <kibettheophilus@gmail.com>
2023-09-25 23:44:03 +0300

    lint

--- payments/src/main/java/com/theophiluskibet/utils/AccessTokenScheduler.java
+++ payments/src/main/java/com/theophiluskibet/utils/AccessTokenScheduler.java
@@ -15,7 +15,10 @@ public class AccessTokenScheduler {
     // TODO: 25/09/2023 change time to 1hr = 1000*60*60 
     @Scheduled(fixedRate = 60 * 60)
     public void getAccessToken() {
-        Request request = new Request.Builder().url("https://sandbox.safaricom.co.ke/oauth/v1/generate?grant_type=client_credentials").addHeader("Authorization", Credentials.basic("bWkjAdbz4wxma05DIsDQh4j1K79qMwAA", "iuOzK7K1fJV9BfFK")).build();
+        Request request = new Request.Builder()
+                .url("https://sandbox.safaricom.co.ke/oauth/v1/generate?grant_type=client_credentials")
+                .addHeader("Authorization", Credentials.basic("bWkjAdbz4wxma05DIsDQh4j1K79qMwAA", "iuOzK7K1fJV9BfFK"))
+                .build();
 
         OkHttpClient okHttpClient = new OkHttpClient();
         okHttpClient.newCall(request).enqueue(new Callback() {
