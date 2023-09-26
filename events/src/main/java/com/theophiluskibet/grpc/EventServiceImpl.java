e15b9f1aa208687f94751ece25611bd8c0e7308c
Kagiri Charles <59829833+Kagiri11@users.noreply.github.com>
2023-09-26 15:26:33 +0300

    Renames get event to event id

--- events/src/main/java/com/theophiluskibet/grpc/EventServiceImpl.java
+++ events/src/main/java/com/theophiluskibet/grpc/EventServiceImpl.java
@@ -5,6 +5,8 @@ import com.theophiluskibet.event.EventsServiceGrpc;
 import io.grpc.stub.StreamObserver;
 import net.devh.boot.grpc.server.service.GrpcService;
 
+import java.lang.reflect.Array;
+
 @GrpcService
 public class EventServiceImpl extends EventsServiceGrpc.EventsServiceImplBase {
     @Override
@@ -14,7 +16,7 @@ public class EventServiceImpl extends EventsServiceGrpc.EventsServiceImplBase {
     }
 
     @Override
-    public void getEvents(EventOuterClass.GetEvent request, StreamObserver<EventOuterClass.EventsReponse> responseObserver) {
+    public void getEvents(EventOuterClass.EventId request, StreamObserver<EventOuterClass.EventsResponse> responseObserver) {
         super.getEvents(request, responseObserver);
     }
 }
