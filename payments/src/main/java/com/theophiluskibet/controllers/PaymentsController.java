020e3a38491df14e8bd1a7cbdd0e77cd8caee7ac
theophiluskibet <kibettheophilus@gmail.com>
2023-10-04 17:45:44 +0300

    update endpoints mapping

--- payments/src/main/java/com/theophiluskibet/controllers/PaymentsController.java
+++ payments/src/main/java/com/theophiluskibet/controllers/PaymentsController.java
@@ -9,7 +9,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/v1/")
+@RequestMapping("/payments/")
 public class PaymentsController {
 
     @Autowired
