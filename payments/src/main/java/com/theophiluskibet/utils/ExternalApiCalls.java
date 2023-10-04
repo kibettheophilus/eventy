c50130753275e660b918aaba1c71c3efbf9f1217
theophiluskibet <kibettheophilus@gmail.com>
2023-10-04 16:11:25 +0300

    add daraja sktpush call

--- payments/src/main/java/com/theophiluskibet/utils/ExternalApiCalls.java
+++ payments/src/main/java/com/theophiluskibet/utils/ExternalApiCalls.java
@@ -2,26 +2,34 @@ package com.theophiluskibet.utils;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.theophiluskibet.dtos.AccessToken;
+import com.theophiluskibet.dtos.InitiateStkPush;
+import com.theophiluskibet.dtos.StkPushRequest;
 import com.theophiluskibet.service.PaymentService;
 import okhttp3.*;
 import org.jetbrains.annotations.NotNull;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
 
 import java.io.IOException;
-import java.time.LocalDateTime;
 
 // TODO: 25/09/2023 cleanup and save token to db
 @Component
-public class AccessTokenScheduler {
+public class ExternalApiCalls {
 
-    @Autowired
-    private PaymentService paymentService;
+//    @Autowired
+//    private PaymentService paymentService;
+
+    private String authHeader;
 
     // TODO: 25/09/2023 change time to 1hr = 1000*60*60
-    @Scheduled(fixedRate = 60 * 60)
-    public void getAccessToken() throws IOException {
+    @Scheduled(fixedRate = 60 * 60 * 1000)
+    private void getAccessToken() throws IOException {
         Request request = new Request.Builder()
                 .get()
                 .url(Constants.accessTokenEndpoint)
@@ -41,11 +49,27 @@ public class AccessTokenScheduler {
                 try (ResponseBody responseBody = response.body()) {
                     ObjectMapper mapper = new ObjectMapper();
                     AccessToken accessToken = mapper.readValue(responseBody.string(), AccessToken.class);
-                    paymentService.saveAccessToken(accessToken);
+                    // paymentService.saveAccessToken(accessToken);
+                    authHeader = accessToken.getAccessToken();
                     System.out.println(accessToken);
                 }
             }
         });
+    }
+
+    public void initiateStkPush(StkPushRequest stkPushRequest) {
+
+        RestTemplate restTemplate = new RestTemplate();
+        HttpHeaders headers = new HttpHeaders();
+        headers.set("Authorization", "Bearer " + authHeader);
+        System.out.println(headers.get("Authorization"));
+        System.out.println(stkPushRequest.getPassword());
+
+        HttpEntity request = new HttpEntity(stkPushRequest, headers);
+
+        ResponseEntity<Object> response = restTemplate.exchange("https://sandbox.safaricom.co.ke/mpesa/stkpush/v1/processrequest", HttpMethod.POST, request, Object.class);
+
+        System.out.println("STK RESPONSE" + response.getBody());
     }
 
 }
