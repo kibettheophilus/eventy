2389abf79ba811b62947f2fca6d78ee4607fb5b0
theophiluskibet <kibettheophilus@gmail.com>
2023-10-04 16:11:06 +0300

    add json names

--- payments/src/main/java/com/theophiluskibet/dtos/StkPushRequest.java
+++ payments/src/main/java/com/theophiluskibet/dtos/StkPushRequest.java
@@ -1,18 +1,30 @@
 package com.theophiluskibet.dtos;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
 
 @Data
 public class StkPushRequest {
+    @JsonProperty("BusinessShortCode")
     private String businessShortCode;
+    @JsonProperty("Password")
     public String password;
+    @JsonProperty("Timestamp")
     private String timeStamp;
+    @JsonProperty("TransactionType")
     private String transactionType;
+    @JsonProperty("Amount")
     private String amount;
+    @JsonProperty("PartyA")
     private String partyA;
+    @JsonProperty("PartyB")
     private String partyB;
+    @JsonProperty("PhoneNumber")
     private String phoneNumber;
+    @JsonProperty("CallBackURL")
     private String callBackUrl;
+    @JsonProperty("AccountReference")
     private String accountReference;
+    @JsonProperty("TransactionDesc")
     private String transactionDesc;
 }
