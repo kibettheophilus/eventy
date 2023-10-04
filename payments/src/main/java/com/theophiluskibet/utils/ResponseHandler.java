1244711883cf61a5bbb9b378af4c5ff5c79e5e07
theophiluskibet <kibettheophilus@gmail.com>
2023-10-04 17:59:20 +0300

    update reponse util

--- payments/src/main/java/com/theophiluskibet/utils/ResponseHandler.java
+++ payments/src/main/java/com/theophiluskibet/utils/ResponseHandler.java
@@ -2,11 +2,13 @@ package com.theophiluskibet.utils;
 
 
 import lombok.Data;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 
 @Data
 public class ResponseHandler {
-    public String code;
-    public Boolean success;
-    public String message;
-    public Object data;
+    private int code;
+    private Boolean status;
+    private String message;
+    private Object data;
 }
