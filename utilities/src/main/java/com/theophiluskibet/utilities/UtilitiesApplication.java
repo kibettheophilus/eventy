78209c18298c506b222745b7c0a91b5a5fa9d778
Kagiri Charles <59829833+Kagiri11@users.noreply.github.com>
2023-10-05 01:54:40 +0300

    Creates utilities module

---
+++ utilities/src/main/java/com/theophiluskibet/utilities/UtilitiesApplication.java
@@ -0,0 +1,13 @@
+package com.theophiluskibet.utilities;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UtilitiesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UtilitiesApplication.class, args);
+    }
+
+}
