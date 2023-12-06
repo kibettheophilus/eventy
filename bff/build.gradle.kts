3c06a5f3dd8a2d76722d59ce6f81b08bd796e31a
theophiluskibet <kibettheophilus@gmail.com>
2023-12-06 14:11:25 +0300

    setup bff module

---
+++ bff/build.gradle.kts
@@ -0,0 +1,28 @@
+plugins {
+    `java-library`
+    id("org.springframework.boot") version "3.1.3"
+    id("io.spring.dependency-management") version "1.1.3"
+}
+
+group = "com.theophiluskibet"
+version = "0.0.1-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation(platform("com.netflix.graphql.dgs:graphql-dgs-platform-dependencies:8.0.0"))
+    implementation("com.netflix.graphql.dgs:graphql-dgs-spring-boot-starter")
+//    implementation("net.devh:grpc-server-spring-boot-starter:2.14.0.RELEASE")
+//    implementation("net.devh:grpc-client-spring-boot-starter:2.14.0.RELEASE")
+//    implementation(project(":proto"))
+
+    testImplementation(platform("org.junit:junit-bom:5.9.1"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
