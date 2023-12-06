5a7723238abc35d09eceed5fd2df075ed5f450ca
theophiluskibet <kibettheophilus@gmail.com>
2023-12-06 14:31:32 +0300

    uncomment deps

--- bff/build.gradle.kts
+++ bff/build.gradle.kts
@@ -21,9 +21,8 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation(platform("com.netflix.graphql.dgs:graphql-dgs-platform-dependencies:8.0.0"))
     implementation("com.netflix.graphql.dgs:graphql-dgs-spring-boot-starter")
-//    implementation("net.devh:grpc-server-spring-boot-starter:2.14.0.RELEASE")
-//    implementation("net.devh:grpc-client-spring-boot-starter:2.14.0.RELEASE")
-//    implementation(project(":proto"))
+    implementation("net.devh:grpc-client-spring-boot-starter:2.14.0.RELEASE")
+    implementation(project(":proto"))
 
     testImplementation(platform("org.junit:junit-bom:5.9.1"))
     testImplementation("org.junit.jupiter:junit-jupiter")
